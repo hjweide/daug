116abed0006cc9017085986ea015db29aa84a271
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-21 02:28:39 +0000

    Adding tarfile member sanitization to extractall()

--- examples.py
+++ examples.py
@@ -16,7 +16,29 @@ def load_cifar10():
         raise IOError('Please download the cifar10 data set to %s.' % path)
 
     with tarfile.open(path, 'r:gz') as f:
-        f.extractall(path='cifar10')
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f, path="cifar10")
     path = join('cifar10', 'cifar-10-batches-py', 'data_batch_1')
     with open(path, 'rb') as f:
         X = pickle.load(f)['data']
